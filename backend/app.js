6e3cb701f6836cdaefa2259858fe51040238067e
husain bandookwala <hello@husainbandookwala.com>
2021-12-03 23:54:18 +0530

    build bug fixes

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-const dotenv = require('dotenv');
+const path = require('path');
 
 const errorMiddleware = require('./middleware/error');
 
